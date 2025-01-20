e4c419af5f440669a339400c426e2a3eb50f0cf5
liamjdavis <davisliam123@gmail.com>
2025-01-20 10:58:21 -0800

    Added gauth and logout features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-//General
 import React from 'react';
-
-//Material UI
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import theme from './theme';
-
-//Pages
 import Studio from './pages/studio';
+import Auth from './pages/auth';
 
-//Contexts
+function PrivateRoute({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const auth = getAuth();
+    return onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  
+  return isAuthenticated ? children : <Navigate to="/" />;
+}
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Studio />;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route 
+            path="/studio" 
+            element={
+              <PrivateRoute>
+                <Studio />
+              </PrivateRoute>
+            } 
+          />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
