580e26c450d65734eec917d85d93cf9c94b55317
liamjdavis <davisliam123@gmail.com>
2025-02-27 14:24:31 -0500

    Fixed definition of 3 blocks

--- src/pages/modules/blockly/blocks/inference/bootstrap_test_prop.js
+++ src/pages/modules/blockly/blocks/inference/bootstrap_test_prop.js
@@ -13,11 +13,14 @@ Blockly.Blocks['bootstrap_test_prop'] = {
       .appendField("Variable:")
       .appendField(new Blockly.FieldDropdown(categorical_vars), "VAR");
     this.appendDummyInput()
-      .appendField("Sample size:")
-      .appendField(new Blockly.FieldNumber(100, 1), "SAMPLE_SIZE");
+      .appendField("Success value:")
+      .appendField(new Blockly.FieldTextInput('"yes"'), "SUCCESS");
     this.appendDummyInput()
       .appendField("Null value:")
       .appendField(new Blockly.FieldNumber(0.5, 0, 1, 0.01), "NULL_VALUE");
+    this.appendDummyInput()
+      .appendField("Sample size:")
+      .appendField(new Blockly.FieldNumber(100, 1), "SAMPLE_SIZE");
     this.appendDummyInput()
       .appendField("Alternative:")
       .appendField(new Blockly.FieldDropdown([
@@ -34,12 +37,53 @@ Blockly.Blocks['bootstrap_test_prop'] = {
   }
 };
 
-// Code generator function
-Blockly.JavaScript['bootstrap_test_prop'] = function(block) {
+// Generic bootstrap test block
+Blockly.Blocks['Gbootstrap_test_prop'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("Bootstrap Test:")
+      .appendField("set.seed(")
+      .appendField(new Blockly.FieldNumber(123, 1, 99999), "SEED")
+      .appendField(")");
+    this.appendDummyInput()
+      .appendField("Variable:")
+      .appendField(new Blockly.FieldTextInput(""), "VAR");
+    this.appendDummyInput()
+      .appendField("Success value:")
+      .appendField(new Blockly.FieldTextInput('"yes"'), "SUCCESS");
+    this.appendDummyInput()
+      .appendField("Data:")
+      .appendField(new Blockly.FieldTextInput(""), "DATASET");
+    this.appendDummyInput()
+      .appendField("Null value:")
+      .appendField(new Blockly.FieldNumber(0.5, 0, 1, 0.01), "NULL_VALUE");
+    this.appendDummyInput()
+      .appendField("Sample size:")
+      .appendField(new Blockly.FieldNumber(100, 1), "SAMPLE_SIZE");
+    this.appendDummyInput()
+      .appendField("Alternative:")
+      .appendField(new Blockly.FieldDropdown([
+        ["less than", "less"],
+        ["greater than", "greater"],
+        ["not equal", "two.sided"]
+      ]), "ALTERNATIVE");
+    
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("Bootstrap test for one proportion");
+  }
+};
+
+// Code generator function for both blocks
+function generateBootstrapTestCode(block) {
   const seed = block.getFieldValue('SEED');
   const variable = block.getFieldValue('VAR');
-  const sampleSize = block.getFieldValue('SAMPLE_SIZE');
+  const success = block.getFieldValue('SUCCESS');
+  const dataset = block.getType() === 'bootstrap_test_prop' ? 'HELPrct' : block.getFieldValue('DATASET');
   const nullValue = block.getFieldValue('NULL_VALUE');
+  const sampleSize = block.getFieldValue('SAMPLE_SIZE');
   const alternative = block.getFieldValue('ALTERNATIVE');
   
   let code = `set.seed(${seed})\n`;
@@ -58,6 +102,9 @@ Blockly.JavaScript['bootstrap_test_prop'] = function(block) {
   }
   
   return code;
-};
+}
+
+Blockly.JavaScript['bootstrap_test_prop'] = generateBootstrapTestCode;
+Blockly.JavaScript['Gbootstrap_test_prop'] = generateBootstrapTestCode;
 
 export default {};
