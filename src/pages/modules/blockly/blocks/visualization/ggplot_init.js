56d1394c120953ad932a92b2406bff77e03de0f8
liamjdavis <davisliam123@gmail.com>
2025-04-30 21:50:26 -0400

    Built ggplot2 blocks

--- src/pages/modules/blockly/blocks/visualization/ggplot_init.js
+++ src/pages/modules/blockly/blocks/visualization/ggplot_init.js
@@ -1,77 +1,42 @@
 import Blockly from 'blockly';
-import { quantitative_vars, categorical_vars } from '../../constants';
+import { quantitative_vars } from '../../constants';
 
+/**
+ * ggplot initialization block definition with dropdown menus for HELPrct
+ */
 Blockly.Blocks['ggplot_init'] = {
   init: function () {
     this.appendDummyInput()
-      .appendField('ggplot(')
-      .appendField('data = ')
-      .appendField(new Blockly.FieldDropdown([['HELPrct', 'HELPrct']]), 'DATA')
-      .appendField(', aes(')
-      .appendField('x = ')
-      .appendField(new Blockly.FieldDropdown([...quantitative_vars, ...categorical_vars]), 'X_VAR')
-      .appendField(', y = ')
-      .appendField(new Blockly.FieldDropdown([...quantitative_vars, ...categorical_vars]), 'Y_VAR')
-      .appendField(', color = ')
-      .appendField(new Blockly.FieldDropdown([['none', 'none'], ...categorical_vars]), 'COLOR_VAR')
-      .appendField('))');
+      .appendField('ggplot(data = HELPrct, mapping = aes(x =')
+      .appendField(
+        new Blockly.FieldDropdown(quantitative_vars),
+        'x_var'
+      )
+      .appendField(', y =')
+      .appendField(
+        new Blockly.FieldDropdown(quantitative_vars),
+        'y_var'
+      )
+      .appendField(')) +');
+    
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(120);
-    this.setTooltip('Initialize a ggplot object with data and aesthetics');
+    this.setTooltip('Initialize a ggplot with HELPrct dataset');
     this.setHelpUrl('https://ggplot2.tidyverse.org/reference/ggplot.html');
   },
 };
 
+/**
+ * Generator for ggplot initialization block
+ */
 Blockly.JavaScript['ggplot_init'] = function (block) {
-  var data = block.getFieldValue('DATA');
-  var x_var = block.getFieldValue('X_VAR');
-  var y_var = block.getFieldValue('Y_VAR');
-  var color_var = block.getFieldValue('COLOR_VAR');
-
-  var code = `ggplot(${data}, aes(x=${x_var}, y=${y_var}`;
-  if (color_var !== 'none') {
-    code += `, color=${color_var}`;
-  }
-  code += '))\n';
-  return code;
-};
-
-Blockly.Blocks['Gggplot_init'] = {
-  init: function () {
-    this.appendDummyInput()
-      .appendField('ggplot(')
-      .appendField('data = ')
-      .appendField(new Blockly.FieldTextInput(''), 'DATA')
-      .appendField(', aes(')
-      .appendField('x = ')
-      .appendField(new Blockly.FieldTextInput(''), 'X_VAR')
-      .appendField(', y = ')
-      .appendField(new Blockly.FieldTextInput(''), 'Y_VAR')
-      .appendField(', color = ')
-      .appendField(new Blockly.FieldTextInput(''), 'COLOR_VAR')
-      .appendField('))');
-    this.setInputsInline(false);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(120);
-    this.setTooltip('Initialize a ggplot object with custom data and aesthetics');
-    this.setHelpUrl('https://ggplot2.tidyverse.org/reference/ggplot.html');
-  },
-};
-
-Blockly.JavaScript['Gggplot_init'] = function (block) {
-  var data = block.getFieldValue('DATA');
-  var x_var = block.getFieldValue('X_VAR');
-  var y_var = block.getFieldValue('Y_VAR');
-  var color_var = block.getFieldValue('COLOR_VAR');
-
-  var code = `ggplot(${data}, aes(x=${x_var}, y=${y_var}`;
-  if (color_var) {
-    code += `, color=${color_var}`;
-  }
-  code += '))\n';
+  var x_var = block.getFieldValue('x_var');
+  var y_var = block.getFieldValue('y_var');
+  
+  var code = 'ggplot(data = HELPrct, mapping = aes(x = ' + x_var + ', y = ' + y_var + ')) +\n';
+  
   return code;
 };
 
